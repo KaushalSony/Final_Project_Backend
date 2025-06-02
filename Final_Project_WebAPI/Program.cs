8f00016f3b1966ab8005b49aab75e0a683479d1c
Kaushal Soni <91665722+KaushalSony@users.noreply.github.com>
2025-06-02 15:46:25 +0530

    Update Program.cs

--- Final_Project_WebAPI/Program.cs
+++ Final_Project_WebAPI/Program.cs
@@ -114,7 +114,7 @@ builder.Services.AddTransient<Final_Project_WebAPI.Services.IEmailService,EmailS
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
